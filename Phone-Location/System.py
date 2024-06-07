fe44ff14ab07541b9f45fab67280da691a5d8458
ElHamudiYausef <proxdggyt@gmail.com>
2024-06-07 18:04:07 +0300

    Add files via upload

---
+++ Phone-Location/System.py
@@ -0,0 +1,23 @@
+import time
+import json
+import subprocess
+
+def get_location():
+    # Termux API kullanarak GPS konumunu alma
+    result = subprocess.run(['termux-location', '--request', 'once', '--provider', 'gps'], stdout=subprocess.PIPE)
+    location = json.loads(result.stdout)
+    latitude = location['latitude']
+    longitude = location['longitude']
+    return latitude, longitude
+
+if __name__ == "__main__":
+    while True:
+        try:
+            latitude, longitude = get_location()
+            google_maps_url = f"https://www.google.com/maps/search/?api=1&query={latitude},{longitude}"
+            print(f"Telefonun konumu: {latitude}, {longitude}")
+            print(f"Google Haritalar URL: {google_maps_url}")
+        except Exception as e:
+            print(f"Konum alınırken hata oluştu: {e}")
+        
+        time.sleep(60)  # Her 60 saniyede bir konumu al ve yazdır
